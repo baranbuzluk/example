0d51a91b119bce96758eb10cdbffff3a6547413b
Baran Buzluk <baranbuzluk@gmail.com>
2023-12-16 22:57:40 +0300

    Multi-Module Spring Boot Application with Gradle Example

---
+++ spring-boot-gradle-multi-module/controller/src/main/java/com/github/baranbuzluk/controller/ExampleController.java
@@ -0,0 +1,14 @@
+package com.github.baranbuzluk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+  @GetMapping("/")
+  public String getIndexPage() {
+    return "Hello Word";
+  }
+
+}
