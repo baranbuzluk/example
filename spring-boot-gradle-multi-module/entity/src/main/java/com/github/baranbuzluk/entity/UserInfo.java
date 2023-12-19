50909e9ed830a87f6e9197dd3cf090c59287fd3f
Baran Buzluk <baran.buzluk@simsoft.com.tr>
2023-12-19 14:12:28 +0300

    User entity ismini UserInfo olarak değiştirir

--- spring-boot-gradle-multi-module/entity/src/main/java/com/github/baranbuzluk/entity/UserInfo.java
+++ spring-boot-gradle-multi-module/entity/src/main/java/com/github/baranbuzluk/entity/UserInfo.java
@@ -1,11 +1,17 @@
 package com.github.baranbuzluk.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
-public class User {
+public class UserInfo {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
     private String firstName;
     private String lastName;
 }
