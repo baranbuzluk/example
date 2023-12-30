ac12cd9214e502b40363ffbe343f2d6266c45dae
Baran Buzluk <baranbuzluk@gmail.com>
2023-12-30 14:26:43 +0300

    Update UserInfo entity

---
+++ spring-boot-gradle-multi-module/entity/src/main/java/com/github/baranbuzluk/entity/User.java
@@ -0,0 +1,25 @@
+package com.github.baranbuzluk.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime createdAt;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime updatedAt;
+
+	private String username;
+	private String password;
+	private String email;
+}
--- spring-boot-gradle-multi-module/repository/src/main/java/com/github/baranbuzluk/UserRepository.java
+++ spring-boot-gradle-multi-module/repository/src/main/java/com/github/baranbuzluk/UserRepository.java
@@ -1,9 +1,10 @@
 package com.github.baranbuzluk;
 
-import com.github.baranbuzluk.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.github.baranbuzluk.entity.User;
+
 @Repository
-public interface UserRepository extends JpaRepository<UserInfo, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
