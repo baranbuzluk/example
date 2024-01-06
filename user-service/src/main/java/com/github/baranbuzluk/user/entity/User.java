1afd335716b1a449be3633c8ce2a8b89d410aa83
Baran Buzluk <baranbuzluk@gmail.com>
2024-01-06 16:33:40 +0300

    Enhance setting  value of createdAt and updatedAt of User entity

--- user-service/src/main/java/com/github/baranbuzluk/user/entity/User.java
+++ user-service/src/main/java/com/github/baranbuzluk/user/entity/User.java
@@ -1,9 +1,9 @@
 package com.github.baranbuzluk.user.entity;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity(name = "users")
@@ -12,8 +12,10 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Integer id;
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private LocalDateTime createdAt;
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private LocalDateTime updatedAt;
 
@@ -21,8 +23,24 @@ public class User {
 	private String password;
 	private String email;
 
-	public User() {
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		throw new UnsupportedOperationException(
+				"When the entity is saved in the database, updatedAt will already be updated!");
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		throw new UnsupportedOperationException(
+				"When the entity is saved in the database, createdAt will be assigned the current time");
+	}
+
+	@PrePersist
+	private void whenSaving() {
 		createdAt = LocalDateTime.now();
 		updatedAt = createdAt;
+	}
+
+	@PreUpdate
+	private void whenUpdating() {
+		updatedAt = LocalDateTime.now();
 	}
 }
